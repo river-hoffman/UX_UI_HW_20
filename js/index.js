23c73346a754324a4f20a93215abe9e152e85395
River Hoffman <riverhoffman22@gmail.com>
2021-02-04 09:05:32 -0500

    what I had in hw 19

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+$('#myResume').click(function(){
+    alert("Hope You Like My Resume");
+})
